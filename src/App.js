f4f4563c8724b6ca5047408c0579bdd5b3e8710e
pratibhaxa <pradhanpratibha28@gmail.com>
2023-02-23 20:02:18 +0530

    commit6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './HomePage';
 import SignUpPage from './SignUpPage';
 
 function App() {
   return(
     <Routes>
-      <Route  path="/" element={<SignUpPage />}></Route>
+      <Route  path="/home-page" element={<HomePage />}></Route>
       <Route  path="/sign-up-page" element={<SignUpPage />}></Route>
     </Routes>
   )
